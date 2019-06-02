e73d8f617fa01b8145670e05ff28c9b155120b9c
Marco Rossi <developer@marco-rossi.com>
2019-06-02 12:13:45 +0200

    Set variable LD_LIBRARY_PATH before build and test

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,3 +1,6 @@
 cert-sync --user $PREFIX/ssl/cacert.pem
 
+PY_LIBDIR=$($PYTHON -c 'import sysconfig; print(sysconfig.get_config_var("LIBDIR"))')
+export LD_LIBRARY_PATH=$PY_LIBDIR:$BUILD_PREFIX/x86_64-conda_cos6-linux-gnu/sysroot/usr/lib/:$LD_LIBRARY_PATH
+
 $PYTHON -m pip install . -vv
