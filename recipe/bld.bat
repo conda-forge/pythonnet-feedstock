dad9f72959f0a42fd8a344a46c5eb1429cb013b8
Ralf Weber <RJMW@users.noreply.github.com>
2019-07-12 22:08:29 +0100

    Set PATH in bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,1 +1,4 @@
+# https://github.com/pythonnet/pythonnet/blob/master/conda.recipe/bld.bat#L4
+set PATH=C:\Program Files (x86)\MSBuild\14.0\Bin;C:\Program Files (x86)\MSBuild\15.0\Bin;%PATH%
+
 %PYTHON% -m pip install . -vv
