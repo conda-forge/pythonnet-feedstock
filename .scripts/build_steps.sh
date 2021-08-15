b7bb4aed7afeb62ca5fdf9b0b98b0eaae2016f89
Marco Rossi <developer@marco-rossi.com>
2021-08-15 19:44:29 +0200

    Use prefix-length of 80

--- .scripts/build_steps.sh
+++ .scripts/build_steps.sh
@@ -52,6 +52,7 @@ if [[ "${BUILD_WITH_CONDA_DEBUG:-0}" == 1 ]]; then
     /bin/bash
 else
     conda $BUILD_CMD "${RECIPE_ROOT}" -m "${CI_SUPPORT}/${CONFIG}.yaml" \
+        --prefix-length 80 \
         --suppress-variables ${EXTRA_CB_OPTIONS:-} \
         --clobber-file "${CI_SUPPORT}/clobber_${CONFIG}.yaml"
     ( startgroup "Validating outputs" ) 2> /dev/null
